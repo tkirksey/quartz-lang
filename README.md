0e1c245885d6b2f719de3e02746757ff538aef1c
Tristan Kirksey <kirkseytc@gmail.com>
2025-04-07 21:36:11 -0400

    setup main.c

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# quartz
+# Quartz
+
+quartz is my attempt to create a programming language that compiles to c code to futher be compiled to machine code.
---
+++ code/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define QUARTZ_VERSION 42025
+
+void print_usage(){
+
+    printf("Usage: quartz [options] file...\n");
+    printf("Options:\n");
+    printf("  -c      Only compiles the code to C\n");
+    printf("  --help  Prints this message.       \n");
+    printf("\n");
+
+}
+
+int main(int argc, char** argv){
+
+    if(argc == 1){
+        printf("Quartz -- version %06d\n", QUARTZ_VERSION);
+        exit(EXIT_SUCCESS);
+    }
+
+    if(strncmp(argv[1], "--help", 7) == 0){
+        print_usage();
+    }
+
+    return 0;
+}
