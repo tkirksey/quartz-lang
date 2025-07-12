e7c418ff81f6ec6dc2d210bb016d0433e34c5f58
kirkseytc <kirkseytc@gmail.com>
2025-07-11 23:51:27 -0400

    july 11th work

--- code/main.c
+++ code/main.c
@@ -42,19 +42,7 @@ int main(int argc, char** argv){
     parse_args(argc, argv, &flag_values, &input_filepaths);
     filter_input_filepaths(&input_filepaths);
     
-    /*
-        validation testing for steps 1
-    */
-
-    // printf("Flags:\n");
-    // printf("    --output:   %s\n", flag_values.output_filename);
-    // printf("    --Werror:   %s\n", WERROR ? "true" : "false");
-    // printf("\n");
-    // printf("Input Filepaths:\n");
-    // for(uint64_t i = 0; i < input_filepaths.input_filepaths_count; i++){
-    //     printf("    %s\n", input_filepaths.input_filepaths_array[i]);
-    // }
-    // printf("\n");
+    
 
     return 0;
 }
